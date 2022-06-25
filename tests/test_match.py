edda5701b5967abca8fa7cb544f044e23b4ab7c4
Daniel Munch <daniellmunch@gmail.com>
2022-06-25 12:56:56 +0200

    updated match testing

--- tests/test_match.py
+++ tests/test_match.py
@@ -22,3 +22,46 @@ def test_match_dataenum():
         (Color.HSL, lambda h, s, l: print(f"HSL: ({h}, {s}, {l})")),
         (Color.HSLA, lambda h, s, l, a: print(f"HSLA: ({h}, {s}, {l}, {a})")),
     )
+    
+    
+def test_match_matchable():
+    class Integer(match.Matchable):
+        def __init__(self, val):
+            self.val = val
+
+        def get_values(self):
+            return [self.val]
+
+        def get_n_values(self):
+            return 1
+
+        def __eq__(self, other):
+            return self.val == other
+
+
+    sixtynine = Integer(69)
+    sixtynine.match((50, lambda val: print("a")), (69, lambda val: print("b", val)))
+
+
+    def cb(x):
+        print("cb", x)
+
+
+    seventy = Integer(70)
+    seventy.match(
+        (70, lambda: print("yeet")),
+        (70, lambda x: print("yay", x)),
+        (70, lambda x, y: print("gg", x, y)),
+        (70, cb),
+    )
+    
+    
+def test_match_dataenum_noargs():
+    Version = dataenum("Version", ("One"), ("Two"), ("Three"))
+
+    version_one = Version.One
+    version_one.match(
+        (Version.One, lambda: print("v1")),
+        (Version.Two, lambda: print("v2")),
+        (Version.Three, lambda: print("v3")),
+    )
