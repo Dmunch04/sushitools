f9f555fe2af8f6a5a208ea8a2e26fd0999e1ca48
Daniel Munch <daniellmunch@gmail.com>
2022-06-25 12:54:18 +0200

    updated dataenum to support new match

--- sushitools/types/dataenum.py
+++ sushitools/types/dataenum.py
@@ -1,101 +1,82 @@
 from typing import List, Tuple
 
 
-# TODO: setattr doesnt work (it just recurses endlessly)
-# TODO: make fields that doesnt take in any args a static field and not a function like that maybe. like a property
-# TODO: im pretty sure the enum fields arent comparable yet. so 2 vars of the same enum field doesnt equal because theyre different objects
+# TODO: setattr doesnt work (it just recurses endlessly) -> i forgot what this issue was about?
+
+
+class staticproperty(staticmethod):
+    def __get__(self, *_):
+        return self.__func__()
+
 
 _enum_template = """\
-from sushitools.cf.match import Matchable, matchable
-
+from match import Matchable
 class {name}(Matchable):
     'DataEnum {name}'
-
     __slots__ = ("__instance")
-
     def __init__(self):
         self.__instance = None
-
     def __getattr__(self, name):
         if self.__instance is None:
             raise AttributeError
-
         if not hasattr(self.__instance, name):
             raise AttributeError
-
         return getattr(self.__instance, name)
-
     def __eq__(self, other):
-        print(other)
-        if not isinstance(other.__instance, self.__instance.__class__):
-            return False
-
-        return self.__instance.COMPARATOR == other.COMPARATOR
-
+        if callable(other):
+            return other.__name__ == type(self.__instance).__name__.split("_")[-1]
+        elif isinstance(other, type(self)):
+            return type(other.__instance).__name__.split("_")[-1] == type(self.__instance).__name__.split("_")[-1]
+        return False
     def __ne__(self, other):
         return not self.__eq__(other)
-
     def __str__(self):
         return self.__instance.__str__()
-
     def get_values(self):
         return self.__instance.get_values()
-
-    @staticmethod
-    def get_n_values():
+    def get_n_values(self):
         return self.__instance.get_n_values()
-
 {cdefs}
-
 {fdefs}
 """
 
 _class_template = """\
     class __{name}_{fname}(Matchable):
         __slots__ = ({fargsq})
-
-        COMPARATOR = {id}
-
         def __init__(self, {fargs}):
 {fassigns}
-
         def __str__(self):
             return "{name}.{fname}({fargs})"
-
+        def __eq__(self, other):
+            raise Exception("idk")
         def get_values(self):
-            return ({fsargs},)
-
-        @staticmethod
-        def get_n_values():
+            return ({fsargs})
+        def get_n_values(self):
             return {fsargs_len}
 """
 
 _field_template = """\
     @staticmethod
-    @matchable(__{name}_{fname})
     def {fname}({fargs}):
         tmp = {name}()
         tmp.__new_{fname}({fargs})
         return tmp
-
     def __new_{fname}(self, {fargs}):
         self.__instance = self.__{name}_{fname}({fargs})
 """
 
 _property_field_template = """\
-    @staticmethod
-    @property
+    @staticproperty
     def {fname}():
         tmp = {name}()
-        tmp.__new_{fname}({fargs})
+        tmp.__new_{fname}()
         return tmp
-
-    def __new_{fname}(self, {fargs}):
-        self.__instance = self.__{name}_{fname}({fargs})
+    def __new_{fname}(self):
+        self.__instance = self.__{name}_{fname}()
 """
 
 
-def dataenum(name: str, fields: List[Tuple[str]]):
+def dataenum(name: str, *fields: List[Tuple[str]]):
     enum_classes = []
     enum_fields = []
 
@@ -103,29 +84,40 @@ def dataenum(name: str, fields: List[Tuple[str]]):
     for field in fields:
         assert len(field) >= 1
 
-        fname = field[0]
-        fargs = ", ".join(field[1:])
+        if len(field) > 1 and (isinstance(field, tuple) or isinstance(field, list)):
+            fname = field[0]
+            fargs = ", ".join(field[1:])
+            args = field[1:]
+        else:
+            fname = field
+            fargs = ""
+            args = []
 
         fassigns = []
-        for arg in field[1:]:
+        for arg in args:
             fassigns.append(f"            self.{arg} = {arg}")
         fassigns = "\n".join(fassigns)
+
+        if not fassigns:
+            fassigns = "            pass"
 
         enum_classes.append(
             _class_template.format(
                 name=name,
                 fname=fname,
-                fargsq=", ".join([f'"{arg}"' for arg in field[1:]]),
-                id=i,
+                fargsq=", ".join([f'"{arg}"' for arg in args]),
                 fargs=fargs,
                 fassigns=fassigns,
-                fsargs=", ".join([f"self.{arg}" for arg in field[1:]]),
-                fsargs_len=len([arg for arg in field[1:]]),
+                fsargs=", ".join([f"self.{arg}" for arg in args])
+                + ("," if len(args) == 1 else ""),
+                fsargs_len=len([arg for arg in args]),
             )
         )
 
-        if len(fargs) > 0:
-            enum_fields.append(_field_template.format(name=name, fname=fname, fargs=fargs))
+        if len(args) > 0:
+            enum_fields.append(
+                _field_template.format(name=name, fname=fname, fargs=fargs)
+            )
         else:
             enum_fields.append(_property_field_template.format(name=name, fname=fname))
 
@@ -137,6 +129,7 @@ def dataenum(name: str, fields: List[Tuple[str]]):
     enum_definition = _enum_template.format(name=name, cdefs=cdefs, fdefs=fdefs)
 
     namespace = dict(__name__="dataenum_%s" % name)
+    namespace["staticproperty"] = staticproperty
     exec(enum_definition, namespace)
     result = namespace[name]
     result._source = enum_definition
