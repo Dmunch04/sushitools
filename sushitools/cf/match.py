f96423d3bef9dc145cd55a5ca319475b766f4219
Daniel Munch <daniellmunch@gmail.com>
2022-06-26 15:05:37 +0200

    added support for match default catch
    i also moved the callback logic to a separate function

--- sushitools/cf/match.py
+++ sushitools/cf/match.py
@@ -5,6 +5,36 @@ def create_value_cb(value: Any) -> Callable:
     return lambda: value
 
 
+def perform_call(cb: Callable, args: List[Any], n_values: int) -> Union[Any, None]:
+    call_n_args = cb.__code__.co_argcount
+    
+    if call_n_args > 0:
+        call_template = "res = call({args})"
+
+        str_args = []
+        for arg in args:
+            if isinstance(arg, str):
+                arg = "'%s'" % arg
+            
+            str_args.append(str(arg))
+
+        arg_difference = call_n_args - n_values
+        if arg_difference < 0: # remove remaining values from argset
+            str_args = str_args[:arg_difference]
+        elif arg_difference > 0: # add missing values to argset
+            for i in range(arg_difference):
+                str_args.append("None")
+
+        f_call = call_template.format(args=",".join(str_args))
+
+        namespace = dict(__name__="match")
+        namespace["call"] = cb
+        exec(f_call, namespace)
+        return namespace["res"]
+    else:
+        return cb()
+
+
 class Matchable(object):
     @classmethod
     def get_values(cls) -> List[Any]:
@@ -14,7 +44,7 @@ class Matchable(object):
     def get_n_values(cls) -> int:
         raise NotImplementedError
 
-    def match(self, *cases: Tuple[Any, Union[Callable, Any]]) -> Union[NoReturn, Any]:
+    def match(self, *cases: Tuple[Any, Union[Callable, Any]], default: Union[Callable, Any, None] = None) -> Union[NoReturn, Any]:
         for case in cases:
             if not (isinstance(case, tuple) or isinstance(case, list)):
                 raise ValueError("case must be a tuple of value and callback or return value")
@@ -24,29 +54,10 @@ class Matchable(object):
 
             comp, call = case
             call = call if callable(call) else create_value_cb(call)
-            
-            call_n_args = call.__code__.co_argcount
 
             if self.__eq__(comp):
-                if call_n_args > 0:
-                    call_template = "res = call({args})"
+                return perform_call(call, self.get_values(), self.get_n_values())
 
-                    str_args = []
-                    for arg in self.get_values():
-                        str_args.append(str(arg))
-
-                    arg_difference = call_n_args - self.get_n_values()
-                    if arg_difference < 0: # remove remaining values from argset
-                        str_args = str_args[:arg_difference]
-                    elif arg_difference > 0: # add missing values to argset
-                        for i in range(arg_difference):
-                            str_args.append("None")
-
-                    f_call = call_template.format(args=",".join(str_args))
-
-                    namespace = dict(__name__="match")
-                    namespace["call"] = call
-                    exec(f_call, namespace)
-                    return namespace["res"]
-                else:
-                    return call()
+        if default:
+            call = default if callable(default) else create_value_cb(default)
+            perform_call(call, self.get_values(), self.get_n_values())
