14b671b9510c0e7b80142c6d1291ba5366153bb2
Dmunch04 <daniellmunch@gmail.com>
2022-10-06 15:41:58 +0200

    removed non useful modules

--- sushitools/__init__.py
+++ sushitools/__init__.py
@@ -3,9 +3,5 @@ __version__ = "0.1.0"
 
 from .cf import *
 from .types import *
-from .extend import extend
 from .primitive import *
 from .json import *
-from .range import *
-from .goto import *
-from .array import *
